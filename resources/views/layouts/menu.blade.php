50a4f018bf1dd5ff2f2740fd95796590ec0fa2bb
Kadal <riz.idn.dev@gmail.com>
2021-09-27 13:23:22 +0700

    initial menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,14 +1,6 @@
-<!-- need to remove -->
 <li class="nav-item">
-    <a href="../widgets.html" class="nav-link">
-        <i class="nav-icon fas fa-th"></i>
-        <p>Widgets</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="gallery.html" class="nav-link">
-        <i class="nav-icon far fa-image"></i>
-        <p>Gallery</p>
+    <a href="#" class="nav-link">
+        <i class="nav-icon far fa-copyright"></i>
+        <p>Merk</p>
     </a>
 </li>
