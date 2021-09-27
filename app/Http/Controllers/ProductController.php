18ceaf84875192e776038f50ff93a39c31828a66
yudawirawan <yudawirawan7@gmail.com>
2021-09-27 15:43:51 +0700

    initial fork

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use DB;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = DB::table('products')->get();
+  
+        return view('products.index',compact('products'));
+    }
 }
---
+++ database/migrations/2021_09_27_081710_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code');
+            $table->string('group_id');
+            $table->string('foto');
+            $table->string('stok_min');
+            $table->string('stok_max');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
